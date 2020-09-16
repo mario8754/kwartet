349a470415dc289c1d5e02966d9d40675d7967a1
Mario <mario.dulder@hotmail.com>
2020-09-17 00:01:22 +0200

    add to do and add card

--- src/components/PlayField.vue
+++ src/components/PlayField.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="playfield">
     <Deck :amount="deck.amount" @draw="draw" />
-    <Hand :cards="player.cards" v-if="deck.amount > 50" />
+    <Hand :cards="player.cards" v-if="deck.amount > 12" />
   </div>
 </template>
 
 <script>
+// Hier logica over het spel in het algemeen
 import Deck from "./Deck.vue";
 import Hand from "./Hand.vue";
 export default {
@@ -17,7 +18,11 @@ export default {
   data() {
     return {
       deck: {
-        amount: 52,
+        amount: 12,
+        // Ik ben nog bij me ma en lucca slaapt dus ik kan niet echt praten
+        // Ik hoor je wel
+        // Dat klopt wel over het algmeen css is meestal een bijproduct
+
         cards: [
           {
             type: "Vogel 1",
@@ -39,11 +44,42 @@ export default {
             type: "Vogel 1",
             value: "K",
           },
+          {
+            type: "Vogel 2",
+            value: "K",
+          },
+          {
+            type: "Vogel 2",
+            value: "K",
+          },
         ],
       },
     };
   },
+  // Mounted wordt aangeroepen wanneer de component zichtbaar is voor de gebruiker.
+  mounted() {
+    // Wanneer de pagina beschikbaar is wil je de deck aanmaken.
+    this.startGame();
+  },
   methods: {
+    startGame() {
+      // Hier zou je je logica van het opstellen van een deck maken.
+      // Ja precies dus de volgorde in deze code zou een beetje zijn
+      this.generateDeck();
+      this.shuffleDeck();
+      this.draw();
+      this.draw();
+      this.draw();
+      this.draw();
+
+      // zoiets.
+    },
+    generateDeck() {
+      // Hier logica om een matrix op te stellen en die dan samen te voegen in 1 array om een deck te krijgen.
+    },
+    shuffleDeck() {
+      // Hier om de deck te random sorteren.
+    },
     draw(amount) {
       this.deck.amount = amount;
       this.player.cards.push({
@@ -52,7 +88,7 @@ export default {
       });
     },
     reset() {
-      this.deck.amount = 52;
+      this.deck.amount = 12;
     },
   },
 };
